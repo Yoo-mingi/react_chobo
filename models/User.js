107df5a2d424f9c3326dcd8356a262004e0a3657
Yoo-mingi <zerotansan@gmail.com>
2022-01-20 07:21:07 +0900

    three

--- models/User.js
+++ models/User.js
@@ -1,36 +1,55 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { ServerClosedEvent } = require('mongodb');
+const { allowedNodeEnvironmentFlags } = require('process');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
-  name : {
-    type : String,
-    maxlength : 50
+  name: {
+    type: String,
+    maxlength: 50
   },
-  email:{
-    type:String,
-    trim:true,
-    unique:1
+  email: {
+    type: String,
+    trim: true,
+    unique: 1
   },
-  password:{
-    type:String,
-    minlength:5
+  password: {
+    type: String,
+    minlength: 5
   },
-  lastname:{
-    type:String,
-    maxlength:50
+  lastname: {
+    type: String,
+    maxlength: 50
   },
-  role:{
-    type:Number,
-    default:0
+  role: {
+    type: Number,
+    default: 0
   },
-  image:String,
-  token:{
-    type:String
+  image: String,
+  token: {
+    type: String
   },
-  tokenExp:{
-    type:Number
+  tokenExp: {
+    type: Number
   }
 })
 
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next()
+      })
+    })
+  }
+  //비밀번호를 암호화 시킨다.
+
+
+})
 const User = mongoose.model('User', userSchema)
 
-module.exports={User}
+module.exports = { User }
